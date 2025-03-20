01be8cb9f55f909527fda55014d91f3707ee5b4f
mounirActualMarketing <your-email@example.com>
2025-03-20 22:47:05 +0000

    update v22

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: [],
+  },
+};
+
+module.exports = nextConfig;
